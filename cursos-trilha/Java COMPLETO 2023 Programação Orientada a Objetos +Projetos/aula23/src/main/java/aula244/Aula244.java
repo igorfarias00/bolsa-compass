ddeca69bed4ec6fef85fb58272bbff42422c8197
Igor Farias <balestfarias@gmail.com>
2024-01-16 15:39:27 -0300

    aula244

---
+++ cursos-trilha/Java COMPLETO 2023 Programação Orientada a Objetos +Projetos/aula23/src/main/java/aula244/Aula244.java
@@ -0,0 +1,26 @@
+package aula244;
+
+import aula239.entities.Product;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Aula244 {
+    public static void main(String[] args) {
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0);
+
+
+
+        for(Product p : set){
+            System.out.println(p);
+        }
+
+        System.out.println(set.contains(prod));
+    }
+}

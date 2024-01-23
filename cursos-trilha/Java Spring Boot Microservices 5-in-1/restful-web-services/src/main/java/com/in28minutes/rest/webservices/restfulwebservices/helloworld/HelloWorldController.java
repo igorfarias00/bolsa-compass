2365dbf2425f72228e92371f20b6fd0ea65e0168
Igor Farias <balestfarias@gmail.com>
2024-01-23 20:08:13 -0300

    aula45

--- cursos-trilha/Java Spring Boot Microservices 5-in-1/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ cursos-trilha/Java Spring Boot Microservices 5-in-1/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,20 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //REST API
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     // /hello-world
 
     //"Hello, World"
@@ -25,6 +35,19 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
+    @GetMapping(path ="/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
+        // - Example: 'en' - English (Good Morning)
+        // - Example: 'nl' - Dutch (Goedemorgen)
+        // - Example: 'fr' - French (Bonjour)
+        // - Example: 'de' - Deutsch (Guten Morgen)
+
+
+        //return "Hello, World V2";
     }
 
 }
